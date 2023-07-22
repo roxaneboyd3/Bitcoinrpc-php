771cd360adfce01c230e6c8991edc31e203f0ed4
roxaneboyd3 <roxaneboyd3@gmail.com>
2023-07-22 04:07:20 -0700

    I added mining info & transaction detail from which we can get nonce after transaction is done

--- index.php
+++ index.php
@@ -3,19 +3,27 @@ require("easybitcoin.php");
 
 $bitcoin = new Bitcoin("wiki", "admin");
 
-
+// In this line of code, I simple get the info about the blockchain in general
 $block = $bitcoin->getblockchaininfo();
-print_r($block);
+//print_r($block);
 
+// In this line of code, I simple get the info about the difficulty of each transaction happening on blockchain
 $difficulty = $bitcoin->getdifficulty();
-print_r($difficulty);
+//print_r($difficulty);
 
-$transaction = $bitcoin->getnewaddress();
-//print_r($transaction);
+//In this line of code, I get info about miner, who do mining the recent block: 
+$mine = $bitcoin-> getmininginfo();
+//print_r($mine);
 
+//transaction to send to the network with signature&wallet after the mining fn finds a nonce with the info) 
+$trans = $bitcoin-> gettransaction();
+//print_r($trans);
+
+//Returns an object containing various state info regarding P2P networking.
 $info = $bitcoin-> getnetworkinfo();
 //print_r($info);
 
-$infop = $bitcoin-> preciousblock();
-//print_r($infop);
+//Returns an object containing various state info regarding P2P networking.
+$prv = $bitcoin-> preciousblock();
+//print_r($prv);
 ?>
